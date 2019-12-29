f3cbf4f0e88f4e9b6fc7ec4c7a5107a26e3bbc31
hzfvictory <38562125+hzfvictory@users.noreply.github.com>
2019-12-29 17:42:42 +0800

    修改端口号

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import http from "http";
 
 import app from "./app";
 
-const PORT = process.env.PORT || 8888;
+const PORT = process.env.PORT ||9000;
 const HOST = process.env.HOST || "0.0.0.0";
 const CB = () => console.log("server object listens on port: %d", PORT);
 
